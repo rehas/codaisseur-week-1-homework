e81436e25095b4bd91d44a2c7996d8f5d4f41781
RehaS <beratson@gmail.com>
2018-07-06 20:49:48 +0200

    Hero Object Tests Pass

--- reha_main.js
+++ reha_main.js
@@ -1,2 +1,12 @@
 // Write your JS here
 console.log("I'm working as a file with new name!");
+
+let hero = {};
+hero.name = "Ember Spirit";
+hero.heroic = true;
+hero.inventory = [];
+hero.health = 10;
+hero.weapon = {
+    type: "Ember Sword",
+    damage : 2
+}
